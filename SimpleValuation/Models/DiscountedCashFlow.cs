8eb3a6c454e494e136eea3a3f1985bb5ecfb86d9
Dylan Richards <dylan.richards81@gmail.com>
2019-11-12 23:21:43 -0500

    Got WACC from scraping the HTML page

---
+++ SimpleValuation/Models/DiscountedCashFlow.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleValuation.Models
+{
+    public class DiscountedCashFlow
+    {
+
+        [JsonProperty("DCF")]
+        public double DCF { get; set; }
+
+    }
+}
--- SimpleValuation/Models/StockModel.cs
+++ SimpleValuation/Models/StockModel.cs
@@ -20,6 +20,7 @@ namespace SimpleValuation.Models
         public CompanyProfile companyProfile = new CompanyProfile();
         public IncomeStatement incomeStatement = new IncomeStatement();
         public EnterpriseValue enterpriseValue = new EnterpriseValue();
+        public DiscountedCashFlow discountedCashFlow = new DiscountedCashFlow();
 
         private static readonly HttpClient client = new HttpClient();
 
@@ -34,7 +35,7 @@ namespace SimpleValuation.Models
             ProcessRepositories().Wait();
 
             this.GrowthRate = 0.05;
-            this.WACC = 0.1005;
+
             this.incomeStatement.financials[0].FreeCashFlow = this.incomeStatement.financials[0].FCFMargin * this.incomeStatement.financials[0].Revenue;
             this.Valuation = this.incomeStatement.financials[0].FreeCashFlow / (this.WACC - this.GrowthRate);
 
@@ -48,14 +49,34 @@ namespace SimpleValuation.Models
             var profileTask = client.GetStringAsync("https://financialmodelingprep.com/api/v3/company/profile/" + StockTicker);
             var incomeTask = client.GetStringAsync("https://financialmodelingprep.com/api/v3/financials/income-statement/" + StockTicker);
             var enterpriseTask = client.GetStringAsync("https://financialmodelingprep.com/api/v3/enterprise-value/" + StockTicker);
+            var dcfTask = client.GetStringAsync("https://financialmodelingprep.com/api/v3/company/discounted-cash-flow/" + StockTicker);
+            var waccTask = client.GetStringAsync("https://financialmodelingprep.com/weighted-average-cost-of-capital/" + StockTicker);
 
             string profileData = await profileTask;
             string incomeData = await incomeTask;
             string enterpriseData = await enterpriseTask;
+            string dcfData = await dcfTask;
+
+            string waccData = await waccTask;
+
+            setWACC(waccData);
 
             this.companyProfile = JsonConvert.DeserializeObject<CompanyProfile>(profileData);
             this.incomeStatement = JsonConvert.DeserializeObject<IncomeStatement>(incomeData);
             this.enterpriseValue = JsonConvert.DeserializeObject<EnterpriseValue>(enterpriseData);
+            this.discountedCashFlow = JsonConvert.DeserializeObject<DiscountedCashFlow>(dcfData);
+        }
+
+        private void setWACC(string strWACC)
+        {
+            int index = strWACC.IndexOf("<tr><th>Wacc</th> <td class=\"align-right\"><input type=\"number\" value=\"");
+       
+            string wacc = strWACC.Substring(index + 70, 5);
+
+            int end = wacc.IndexOf("\"");
+            if(end != -1) wacc = wacc.Substring(0, end);
+
+            this.WACC = double.Parse(wacc) / 100;
         }
 
     }
